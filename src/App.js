363845cca95859b3b5e42993c64cb2e83f7f4669
Alan Nunes Cerqueira <alannunes90@gmail.com>
2019-07-19 12:17:11 -0300

    add https

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HttpsRedirect from 'react-https-redirect';
+
 function App() {
   return (
+    <HttpsRedirect>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +23,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    </HttpsRedirect>
+    );
 }
 
 export default App;
